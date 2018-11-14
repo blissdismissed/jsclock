20f0f311699f070eb10d10f8ed820958e4cf250a
blissdismissed <ajautz@gmail.com>
2018-11-14 15:46:03 -0500

    Add js function to update the hr,min, and sec hands.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log(hr, min, sec);
+
+let hrPosition = 360 / 12 * hr + (min * (360 / 60) / 12);
+let minPosition = 360 / 60 * min + (sec * (360 / 60) / 60);
+let secPosition = 360 / 60 * sec;
+
+function runTheClock() {
+
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+(360/60);
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
